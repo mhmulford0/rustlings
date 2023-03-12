409e5e076c0db673bdcef6898953556bfdd95084
Matthew Mulford <mhmulford0@gmail.com>
2023-03-12 18:14:15 -0500

    rusty

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,11 +1,12 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
-// Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8,u8,u8),
+    Echo(String),
+    Move{x: u8, y: u8},
+    Quit
 }
 
 struct Point {
@@ -37,6 +38,17 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+       
+        match message {
+            Message::ChangeColor(r,g,b) => self.change_color((r,g,b)),
+            Message::Echo(str) => self.echo(str),
+          
+            Message::Move{x, y} => self.move_position(Point{x,y}),
+
+            Message::Quit => self.quit(),
+            
+        }
+
         // TODO: create a match expression to process the different message variants
         // Remember: When passing a tuple as a function argument, you'll need extra parentheses: fn function((t, u, p, l, e))
     }
@@ -55,7 +67,7 @@ mod tests {
         };
         state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
+         state.process(Message::Move{ x: 10, y: 15 });
         state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
