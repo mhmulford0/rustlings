a204aa612fe647163a31d35ce347ed4afed06ea2
Matthew Mulford <mhmulford0@gmail.com>
2023-03-25 17:01:18 -0500

    thru quiz 2

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,7 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -37,6 +36,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        if !basket.contains_key(&fruit) {
+            let count = match fruit{
+                Fruit::Pineapple => 5,
+                Fruit::Banana => 3,
+                _ => 0
+            };
+            basket.insert(fruit, count);
+        }
     }
 }
 
